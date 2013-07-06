000ed0cbe9b3baf2b6142bca0dc1d94912a161e6
Alpha Liu <alpha.liu@me.com>
2013-07-06 22:06:43 +0800

    Initial project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toal::Application.config.secret_key_base = '6db58a35fbaaf10214dc1826e7817475ef9e5d52b710285236d6c44bc1cec8c61807da486d1179c549444b26334b4b6426e1d4272e0e62eb634448ebd0e48c08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toal::Application.config.session_store :cookie_store, key: '_toal_session'
